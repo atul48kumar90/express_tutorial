973b977d476e553baa72ae30de111329c0cd6cdb
Atul Kr Karn <atul48kumar90@gmail.com>
2022-10-05 12:54:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+const members = require('./Members');
+
+
+const logger = (req, res, next) => {
+    console.log("Hello");
+    next();
+}
+
+app.use(logger);
+  
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started at ${PORT}`));
